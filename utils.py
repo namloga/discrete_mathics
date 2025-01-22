b89f989ed1b060f8008e3f8455ec46204cbc73ef
namloga <namloga2004@gmail.com>
2025-01-22 16:24:30 +0300

    finsh lab

--- utils.py
+++
@@ -1,7 +0,0 @@
-def read_file(file_name):
-    with open(file_name, 'r') as file:
-        return file.read()
-
-def write_file(file_name, content):
-    with open(file_name, 'w', encoding='utf-8') as file:
-        file.write(content)
